0d2a0b7f9c1e5048e0a9c318067367e84854db5a
Ajit-143 <ajitinfo143@gmail.com>
2023-12-19 11:14:21 +0530

    chnge 3

--- varibale.js
+++ varibale.js
@@ -1,1 +1,12 @@
-console.log("wjfbj")
+console.log("Variable")
+let a=23;
+let b= "why";
+console.log(a)
+console.log(b)
+
+/*
+three type of varibale
+1. var - which we will not use
+2. let - can use
+3. const - regularly use
+*/
