5253db863c46ee27d423a51973f72fb56b222529
Ajit-143 <ajitinfo143@gmail.com>
2023-12-19 12:04:02 +0530

    practice

---
+++ practice1.js
@@ -0,0 +1,33 @@
+//1 
+let a="Ajit   "
+let b= 2;
+console.log(a+b)
+
+//2
+console.log(typeof (a+b))
+console.log(typeof a)
+console.log(typeof b)
+
+//3
+const student= {
+    name: "Ajit",
+    uid:"20bcs5501",
+    ispass :true
+
+}
+   // student="choco" // not possilbe u cant chnage const 
+
+    //4 
+
+    student['friend']="chiku"
+    console.log(student)
+
+    //5
+    const dict={
+        ajit:"cant be defaeted",
+        uid:"admission no",
+        course:"cse",
+        status:"single"
+
+    }
+    console.log(dict.status)
