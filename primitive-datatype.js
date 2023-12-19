05e5c14be901d5d7af95fef7f2445c9c921f502f
Ajit-143 <ajitinfo143@gmail.com>
2023-12-19 11:19:12 +0530

    predatatype

---
+++ primitive-datatype.js
@@ -0,0 +1,11 @@
+// nn bb ss u
+
+let a = null; // null
+let b= 123; // number
+let c= true; // boolean
+let d= BigInt("23434")  // Bigint
+let e= "Ajit"  //string
+let f=Symbol("hi how r u")  // symbol
+let g =undefined // undefined
+
+// there r 7 primitive data type 
