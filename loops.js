e07839e1ef396e7c2b11c01553daba5a68d7ecd0
Ajit-143 <ajitinfo143@gmail.com>
2023-12-20 17:04:36 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,19 @@
+for(let i=1; i<=2; i++){
+    console.log(i)
+}
+
+let obj={
+    marks:90,
+    name:"Ajit",
+    bheem:"chutki"
+
+}
+for(let a in obj){
+    console.log(a + "  " + obj[a])//for in loop 
+}
+
+for (let b of "Ajit"){
+    console.log(b) // for of loop
+}
+
+// while nd do while 
