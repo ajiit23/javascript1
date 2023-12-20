4bc697b24f9ad3cd23053778c1f30c1bf664fae5
Ajit-143 <ajitinfo143@gmail.com>
2023-12-20 18:54:56 +0530

    prct3

---
+++ practice3.js
@@ -0,0 +1,30 @@
+//1,2
+/*
+let obj={
+    ajit:56,
+    chiku:19,
+    choco:99
+}
+for(let i in obj){
+    console.log(i +" "+ obj[i])
+}
+
+//3
+
+let cn=99
+let i= prompt("enter a number")
+
+while(i!=cn){
+    i=prompt("try again")
+   
+}
+alert("correct")
+*/
+//4
+
+const mean=(a,b,c,d,e)=>{
+return (a+b+c+d+e)/5
+}
+
+
+console.log(mean(2,2,2,2,2))
