46e971d4839a64787e3b5b5a22049f7f1a441123
Ajit-143 <ajitinfo143@gmail.com>
2023-12-20 18:06:37 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,11 @@
+function fun(x,y){
+    return  (1/2*x*y) + " "+ "A"
+}
+
+const hii=()=>{
+    console.log("hii") // another way of making funtion ( currently used in industry)
+}
+let a=5,b=2
+console.log(fun(a,b))
+
+hii() // invoking func 
