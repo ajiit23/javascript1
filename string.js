b46b8e59860b18c8b9136223b54d8c9f149a13e9
Ajit-143 <ajitinfo143@gmail.com>
2023-12-21 12:41:36 +0530

    string

---
+++ string.js
@@ -0,0 +1,26 @@
+// String is IMMUTALBLE
+
+let name1="Ajit 20BCS5501"
+let name2='mmm'
+console.log(name1.length)
+console.log(name1[0])
+
+let a= "Ajit"
+let b="ceedede"
+
+let res=`${a} is a friend of ${b}`
+console.log(res)
+
+// esccape sequence character
+let fruit = 'bana\'na'
+console.log(fruit)
+
+console.log(name1.toUpperCase())
+console.log(name1.toLowerCase())
+console.log(name1.slice(1,3))
+console.log(name1.slice(3))
+console.log(name1.replace("A","P"))
+
+for(i=0;i<name1.length;i++){
+    console.log(name1[i])
+}
